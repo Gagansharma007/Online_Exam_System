bdf60114b5e8a1086e1f4b05439e5d463b8ac3ae
Gagan sharma <sharmagagan01234@gmail.com>
2024-05-25 14:48:06 +0530

    server folder updated

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log("Database connection successful.");
+}).catch((err)=>{
+    console.log(err.message);
+});
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
-app.get('/',(req, res)=> res.send('hello world this is new text.'));
+app.use('/api/auth',userRoutes);
 const port = process.env.PORT || 5000 ;
-app.listen(port, ()=> console.log('server started'));
+const server = app.listen(port, ()=> console.log(`server started at ${port}`));
